3b7bc6b4a8c18f147d642ba16afb14e1bbfffc64
palomamiranda2003-source <palomamiranda2003@gmail.com>
2025-10-28 14:45:06 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import flet as ft
+from login import login_view
+
+def main(page: ft.Page):
+    def route_change(e):
+        page.views.clear()
+        if page.route == "/":
+            page.views.append(login_view(page))
+        #elif page.route == "/dashboard":
+        #    page.views.append(main_menu_view(page))
+        #elif page.route == "/mapa":
+        #    page.views.append(mapa_view(page))
+        #elif page.route == "/perfil":
+        #    page.views.append(perfil_view(page))
+        page.update()
+
+    page.on_route_change = route_change
+    page.go("/")  # arranca en login
+
+ft.app(target=main)
