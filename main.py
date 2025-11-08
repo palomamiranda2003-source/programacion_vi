d5ac3a4c83049783293409780166c44a32e348c8
palomamiranda2003-source <palomamiranda2003@gmail.com>
2025-11-08 11:28:58 -0300

    Update main.py
    se agregó la redirección a la nueva interfaz del mapa

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import flet as ft
 from login import login_view
 from dashboard import main_menu_view 
 from perfil import perfil_view
+from mapa import mapa_view
 
 def main(page: ft.Page):
     def route_change(e):
         page.views.clear()
         if page.route == "/":
             page.views.append(login_view(page))
-        #estas son las vistas que se crearán más adelante
         elif page.route == "/dashboard":
             page.views.append(main_menu_view(page))
-        #elif page.route == "/mapa":
-        #    page.views.append(mapa_view(page))
+        elif page.route == "/mapa":
+            page.views.append(mapa_view(page))
         elif page.route == "/perfil":
             page.views.append(perfil_view(page))
         page.update()
