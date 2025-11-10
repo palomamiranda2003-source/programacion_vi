e247566b4c0b144a2ddadb7f512dc5489afd45c2
palomamiranda2003-source <palomamiranda2003@gmail.com>
2025-11-10 13:07:23 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,1 +1,15 @@
-# En desarrollo
+import flet as ft
+import webbrowser
+
+def mapa_view(page: ft.Page):
+
+    #en desarrollo
+
+    return ft.View(
+        "/mapa",
+        [
+            ft.Container(
+                
+            )
+        ],
+    )
